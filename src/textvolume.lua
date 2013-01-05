3f5b528d33e3bd97ee26bac3793ef931952d426e
Josh Komoroske <jdkomo.dev@gmail.com>
2013-01-04 21:37:31 -0500

    Defined basic API interface
    - All methids are currently empty.

--- src/textvolume.lua
+++ src/textvolume.lua
@@ -7,19 +7,53 @@ local capi = { timer = timer }
 -- awful.widget.textvolume
 local textvolume = { mt = {} }
 
-function textvolume.new(timeout)
-    local timeout = timeout or 5
+function textvolume.new(channel, timeout)
+	local channel = channel or "Master"
+	local timeout = timeout or 5
 
-    local w = textbox()
-    local timer = capi.timer { timeout = timeout }
-    timer:connect_signal("timeout", function() w:set_markup("vol --") end)
-    timer:start()
-    timer:emit_signal("timeout")
-    return w
+	local w = textbox()
+	local timer = capi.timer { timeout = timeout }
+	timer:connect_signal("timeout", function() w:set_markup("vol --") end)
+	timer:start()
+	timer:emit_signal("timeout")
+	return w
 end
 
+
+function textvolume:get()
+end
+
+
+function textvolume:set(val)
+end
+
+
+function textvolume:inc(val)
+end
+
+
+function textvolume:dec(val)
+end
+
+
+function textvolume:mute()
+end
+
+
+function textvolume:unmute()
+end
+
+
+function textvolume:toggle()
+end
+
+
+function textvolume:update()
+end
+
+
 function textvolume.mt:__call(...)
-    return textvolume.new(...)
+	return textvolume.new(...)
 end
 
 return setmetatable(textvolume, textvolume.mt)
